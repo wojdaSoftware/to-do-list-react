2f185dee8240dcd0d254f06fd21e5d4e6553dd1b
Kacper Wojda <wojda.kacper@gmail.com>
2023-10-02 18:07:48 +0200

    slight refactoring

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './style.css';
 
-const Buttons = ({ tasks, hideFinished }) => {
-    return tasks.length > 0 && (
+const Buttons = ({ tasks, hideFinished }) => (
+    tasks.length > 0 && (
         <div className="gridContainer">
             <button className="gridContainer__button">
                 {hideFinished ? "Show" : "Hide"} finished
@@ -15,5 +15,5 @@ const Buttons = ({ tasks, hideFinished }) => {
             </button>
         </div>
     )
-};
+);
 export default Buttons;
