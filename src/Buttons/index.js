5a1d8a641eca00adfa8cb394efeaff326199bebe
Kacper Wojda <wojda.kacper@gmail.com>
2023-10-02 17:35:07 +0200

    slight refactoring

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './style.css';
 
 const Buttons = ({ tasks, hideFinished }) => {
-    if (tasks.length > 0) {
-        return (
-            <div className="gridContainer">
-                <button className="gridContainer__button">{hideFinished ? "Show" : "Hide"} finished</button>
-                <button className="gridContainer__button" disabled={tasks.every(({ done }) => done)}>Finish all</button>
-            </div>
-        )
-    } else {
-        return (
-            <div className="gridContainer" />
-        )
-    };
-}
+    return tasks.length > 0 && (
+        <div className="gridContainer">
+            <button className="gridContainer__button">
+                {hideFinished ? "Show" : "Hide"} finished
+            </button>
+            <button
+                className="gridContainer__button"
+                disabled={tasks.every(({ done }) => done)}
+            >
+                Finish all
+            </button>
+        </div>
+    )
+};
 export default Buttons;
