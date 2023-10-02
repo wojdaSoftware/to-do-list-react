ddcdbf231fe62fbefb39bfde39d477b1e793c2bd
Kacper Wojda <wojda.kacper@gmail.com>
2023-10-02 19:05:39 +0200

    moved header to a component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Form from './Form';
 import List from './List';
 import Buttons from './Buttons';
 import Section from './Section';
+import Header from './Header';
 
 const tasks = [
   { id: 1, content: "Spać", done: false },
@@ -12,26 +13,22 @@ const hideFinished = false;
 
 function App() {
   return (
-    <React.Fragment>
-      <header className="header">
-        <h1>To-do list</h1>
-      </header>
-      <main className="main">
-        <Section
-          title={"Add a new task"}
-          body={<Form />}
-        />
-        <Section
-          title={"Task list"}
-          extraHeaderContent={
-            <Buttons tasks={tasks} hideFinished={hideFinished} />
-          }
-          body={
-            <List tasks={tasks} hideFinished={hideFinished} />
-          }
-        />
-      </main>
-    </React.Fragment>
+    <main className="main">
+      <Header title={"To-do list"} />
+      <Section
+        title={"Add a new task"}
+        body={<Form />}
+      />
+      <Section
+        title={"Task list"}
+        extraHeaderContent={
+          <Buttons tasks={tasks} hideFinished={hideFinished} />
+        }
+        body={
+          <List tasks={tasks} hideFinished={hideFinished} />
+        }
+      />
+    </main>
   );
 }
 
