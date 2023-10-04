cdd00d9b48cf478eef785a1465000f2f69eb5252
Kacper Wojda <wojda.kacper@gmail.com>
2023-10-04 22:46:12 +0200

    finishing tasks

--- src/App.js
+++ src/App.js
@@ -21,6 +21,15 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const markTask = (id) => {
+    setTasks(tasks => tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, done: !task.done }
+      };
+      return task;
+    }));
+  };
+
   return (
     <Container>
       <Header title={"To-do list"} />
@@ -42,6 +51,7 @@ function App() {
             tasks={tasks}
             hideFinished={hideFinished}
             removeTask={removeTask}
+            markTask={markTask}
           />
         }
       />
--- src/List/index.js
+++ src/List/index.js
@@ -1,13 +1,18 @@
 import './style.css';
 
-const List = ({ tasks, hideFinished, removeTask }) => (
+const List = ({ tasks, hideFinished, removeTask, markTask }) => (
     <ul className="list" >
         {tasks.map(task => (
             <li
                 key={task.id}
                 className={`list__item ${task.done && hideFinished ? "list__item--hidden" : ""}`}
             >
-                <button className="list__button">{task.done ? "✔" : ""}</button>
+                <button
+                    className="list__button"
+                    onClick={() => markTask(task.id)}
+                >
+                    {task.done ? "✔" : ""}
+                </button>
                 <p className={`list__paragragh ${task.done ? "list__paragraph--marked" : ""}`}>{task.content}</p>
                 <button
                     className="list__button list__button--second"
