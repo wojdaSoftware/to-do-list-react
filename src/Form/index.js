77e04490a022b50fbe0e41f43c49f909dfde51c8
Kacper Wojda <wojda.kacper@gmail.com>
2023-11-14 16:33:36 +0100

    added a useRef to set focus on input after submit

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,18 +1,25 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Button, Input, Wrapper } from './styled.js';
 
 const Form = ({ addTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef(null)
+
+    const focusInput = () => {
+        inputRef.current.focus();
+    };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
         addTask(newTaskContent.trim());
         setNewTaskContent("");
-    }
+        focusInput();
+    };
 
     return (
         <Wrapper onSubmit={onFormSubmit}>
             <Input
+                ref={inputRef}
                 placeholder="What has to be done?"
                 value={newTaskContent}
                 onChange={(event) => setNewTaskContent(event.target.value)}
