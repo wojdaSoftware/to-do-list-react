89289a4c89c200a2f260f2a3748ce843ac656c98
Kacper Wojda <wojda.kacper@gmail.com>
2024-01-04 09:54:33 +0100

    deleted useTasks hook

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -3,37 +3,24 @@ import List from './List';
 import Buttons from './Buttons';
 import Section from '../../common/Section';
 import Header from '../../common/Header';
-import useTasks from '../../useTasks';
 
-function Tasks() {
-  const {
-    // tasks,
-    // hideFinished,
-    // toggleHideFinished,
-    // markTask,
-    // markAllTasks,
-    // addTask,
-    // removeTask
-  } = useTasks();
-
-  return (
-    <>
-      <Header title={"To-do list"} />
-      <Section
-        title={"Add a new task"}
-        body={<Form />}
-      />
-      <Section
-        title={"Task list"}
-        extraHeaderContent={
-          <Buttons />
-        }
-        body={
-          <List />
-        }
-      />
-    </>
-  );
-}
+const Tasks = () => (
+  <>
+    <Header title={"To-do list"} />
+    <Section
+      title={"Add a new task"}
+      body={<Form />}
+    />
+    <Section
+      title={"Task list"}
+      extraHeaderContent={
+        <Buttons />
+      }
+      body={
+        <List />
+      }
+    />
+  </>
+);
 
 export default Tasks;
