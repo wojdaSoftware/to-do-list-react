73bdcb4c9d968946fbbe190571f18a100c8b3e70
Kacper Wojda <wojda.kacper@gmail.com>
2024-02-15 13:01:54 +0100

    padding adjustments

--- src/common/Button/styled.js
+++ src/common/Button/styled.js
@@ -5,7 +5,9 @@ export const ButtonComponent = styled.button`
     background-color: white;
     border: none;
     padding: 0;
+    padding-right: 20px;
     transition: 500ms;
+    text-align: right;
 
     &:hover {
     color: ${({theme})=> theme.colors.activeColor};
