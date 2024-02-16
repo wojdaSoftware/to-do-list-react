017586f018e2fc40fa95f6d9d3e3d63fec490194
Kacper Wojda <wojda.kacper@gmail.com>
2024-02-16 12:35:25 +0100

    noMargin prop

--- src/features/tasks/TasksPage/Form/styled.js
+++ src/features/tasks/TasksPage/Form/styled.js
@@ -1,10 +1,10 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Wrapper = styled.form`
     padding: 20px;
     display: flex;
 
-    @media (max-width: ${({theme})=> theme.breakpoint}px) {
+    @media (max-width: ${({ theme }) => theme.breakpoint}px) {
         flex-wrap: wrap;
     }
 `;
@@ -16,23 +16,27 @@ export const Input = styled.input`
     flex-grow: 1;
     margin-right: 20px;
 
-    @media (max-width: ${({theme})=> theme.breakpoint}px) {
+    @media (max-width: ${({ theme }) => theme.breakpoint}px) {
         flex-basis: 100%;
         margin-right: 0px;
         margin-bottom: 10px;
     }
+
+    ${({noMargin}) => noMargin && css`
+        margin-right: 0;
+    `}
 `;
 
 export const Button = styled.button`
     border: none;
     border-radius: 0;
-    background-color: ${({theme})=> theme.colors.primaryColor};
+    background-color: ${({ theme }) => theme.colors.primaryColor};
     color: white;
     padding: 12px;
     transition: background 500ms 200ms, transform 500ms;
 
     &:hover {
-        background-color: ${({theme})=> theme.colors.activeColor};
+        background-color: ${({ theme }) => theme.colors.activeColor};
         transform: scale(1.2);
     }
 
@@ -41,7 +45,7 @@ export const Button = styled.button`
         transition-duration: 0ms;
     }
 
-    @media (max-width: ${({theme})=> theme.breakpoint}px) {
+    @media (max-width: ${({ theme }) => theme.breakpoint}px) {
         flex-basis: 100%;
 
         &:hover {
